21c8696f866fc0e191c3dd928522037992063d26
Garima Jain <garimaj2108@gmail.com>
2017-05-13 11:15:10 -0700

    Added pseudocode for decrypt

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -5,7 +5,7 @@
 # After that we need to work with edge cases by giving an if Conditional
 # Then we will use elsif to determine what to do when we encounter " "
 # And if none of the above conditions occur then we will simply iterate through the loop and store the next alphabet in place of the current one.
-# ------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
 # taking a parameter input here
 def encrypt (input)
@@ -29,4 +29,15 @@ def encrypt (input)
 end
   input
 end
+
+# pseudocode
+# Now we will define decrypt method
+# Here, we will take a parameter, and test it for the following conditions
+# To iterate through the string we need to compare it against the length of the parammeter, where index is initialized at 0
+# Here we will also declare a alphabet variable so as to compare the input's position against alphabet
+# Now we will store the index of the input against the alphabet's index
+# Then we will use elsif to determine what to do when we encounter " "
+# Now we will subtract the stored index with 1 and store it
+# ------------------------------------------------------------
+
 
