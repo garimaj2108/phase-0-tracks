40d60bea3a5c0ccb57801809266831aacb61d08c
Garima Jain <garimaj2108@gmail.com>
2017-05-20 00:04:32 -0700

    Updated comments

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,3 +1,14 @@
+# This program accepts user input and stores it in a hash
+# Pseudocode:
+# First declare an empty hash as you need to take user input and store it in the hash
+# Then ask user to enter their client's name, age, email, no.of cildren and decor theme
+# then iterate through the hash and printout the results
+#Then ask the user if they would like to update any details
+# If user says yes, then take input for updates and update in hash
+# then iterate through the hash and printout the results
+
+
+# Declaring an empty hash
 client_details = {}
 
 puts "Welcome to Interior Design Client Solutions!\n"
@@ -18,11 +29,12 @@ puts "Please enter the following details to start with your client diaries!"
   puts "Decor Theme:"
   client_details [:decor] = gets.chomp
 
+#Iterating through each key,value pair and printing it out
 client_details.each do|key,value|
 puts "#{key} => #{value}"
 end
 
-
+# Asking the user, if they would like to update the form
 puts "Would you like to update the form? (yes / no)"
 input = gets.chomp
 if input == "yes"
@@ -34,6 +46,7 @@ if input == "yes"
   client_details[form_field] = update
 end
 
+#Iterating through each key,value pair and printing it out
 client_details.each do|key,value|
 puts "#{key} => #{value}"
 end
