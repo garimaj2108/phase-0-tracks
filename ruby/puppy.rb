ce80b0bb27454674989b4b563ddaecfe82aa584e
Garima Jain <garimaj2108@gmail.com>
2017-05-23 22:52:49 -0700

    Added species type, characteristics and behavior of puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+=begin
+SPECIES --> TERRIER
+
+CHARACTERISTICS
+Name: Yorkshire Terrier
+Coat Color: Dark grey
+Friendly: yes
+Eye Count: 2
+Hair: Straight
+Temperament: Over protective
+
+BEHAVIOR
+Jump
+Bark
+Lick
+Tail wagging
+=end
