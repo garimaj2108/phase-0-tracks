d37fbd3e8bca65abbfef8654cee4d874af291f1b
Garima Jain <garimaj2108@gmail.com>
2017-05-31 19:01:21 -0700

    Added a pseudocode

---
+++ js/explore.js
@@ -0,0 +1,3 @@
+/*
+for every string input, loop through the string in a descending fashion,and store each looped character in a new variable and add to it our character at that index, so as to get a reverse string.
+*/
