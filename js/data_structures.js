10d48036632aec4bd73a957e0b3d7c417ff1c2f0
Garima Jain <garimaj2108@gmail.com>
2017-06-01 09:35:32 -0700

    Added arrays for Release 0

---
+++ js/data_structures.js
@@ -0,0 +1,10 @@
+var colors = ["Brown", "Black", "Yellow", "White"]
+var name = ["Ed", "Leggy", "Long Tail", "Snow white"]
+
+console.log("The original list contains horses of the following colors: \n" +colors)
+colors.push("Grey")
+console.log("Now the list is: \n" +colors +"\n")
+
+console.log("The original list contains the following horses: \n" +name)
+name.push("Gandalf the grey")
+console.log("Now the list is: \n" +name +"\n")
