4537b47e708bf32b6fc0d5099bc52e00487d3856
Garima Jain <garimaj2108@gmail.com>
2017-06-01 11:43:18 -0700

    Release1: Build an object

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,10 +1,24 @@
-var colors = ["Brown", "Black", "Yellow", "White"]
-var name = ["Ed", "Leggy", "Long Tail", "Snow white"]
+// A color array
+var colors = ["Brown", "Black", "Yellow", "White"];
+// A array of horse names
+var horse_name = ["Ed", "Leggy", "Long Tail", "Snow white"];
 
-console.log("The original list contains horses of the following colors: \n" +colors)
-colors.push("Grey")
-console.log("Now the list is: \n" +colors +"\n")
+console.log("The original list contains horses of the following colors: \n" +colors);
+// Adding color to the 'colors' array
+colors.push("Grey");
+console.log("Now the list is: \n" +colors +"\n");
 
-console.log("The original list contains the following horses: \n" +name)
-name.push("Gandalf the grey")
-console.log("Now the list is: \n" +name +"\n")
+console.log("The original list contains the following horses: \n" +horse_name);
+// Adding a name to 'horse_name' array
+horse_name.push("Gandalf");
+console.log("Now the list is: \n" +horse_name +"\n");
+
+// Declaring a empty object
+var barn = {};
+
+// looping through, to assign a color to each horse
+for (i=0; i < colors.length; i++){
+  barn[horse_name[i]] = colors[i];
+}
+
+console.log(barn);
