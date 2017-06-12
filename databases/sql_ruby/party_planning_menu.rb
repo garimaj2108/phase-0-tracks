113213c1a3eb92c2dc2d641fc4b0a5546e91ed72
Garima Jain <garimaj2108@gmail.com>
2017-06-12 08:50:16 -0700

    Update tables and defined display functions

--- databases/sql_ruby/party_planning_menu.rb
+++ databases/sql_ruby/party_planning_menu.rb
@@ -1,19 +1,19 @@
-# OPERATION PARTY PLANNING MENU!
-# This interface is designed to help you with any party that you host.
-# This interface will provide users with menu options
+# OPERATION MENU MANAGAEMENT!
+# This interface is designed to help any event manager or a restaurateur manage the menus.
+# This interface will provide users with options to add items to teh menu and make updates to them
 
-# require gems
+# Required gems
 require 'sqlite3'
 require 'faker'
 
-# create SQLite3 database
-
+# Create SQLite3 database
 db = SQLite3::Database.new("party_menu.db")
-db.results_as_hash = true
+#db.results_as_hash = true
 
 
-# learn about fancy string delimiters
+# Fancy string delimiters
 
+# This table stores appetizer unique id, appetizer names and its description
 create_table = <<-SQL
   CREATE TABLE IF NOT EXISTS appetizers(
     appetizer_id INTEGER PRIMARY KEY,
@@ -22,6 +22,7 @@ create_table = <<-SQL
   )
 SQL
 
+# This table stores main_course unique id, main_course names and its description
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS main_course(
     main_course_id INTEGER PRIMARY KEY,
@@ -30,6 +31,7 @@ create_table_cmd = <<-SQL
   )
 SQL
 
+# This table stores dessert unique id, dessert names and its description
 create_table_cmd_1 = <<-SQL
   CREATE TABLE IF NOT EXISTS dessert(
     dessert_id INTEGER PRIMARY KEY,
@@ -38,6 +40,7 @@ create_table_cmd_1 = <<-SQL
   )
 SQL
 
+# This table stores menu unique id, and references foreign key appetizer_id of appetizertable, main_course_id of main_course table and dessert_id of dessert table.
 create_table_cmd_2 = <<-SQL
   CREATE TABLE IF NOT EXISTS menu(
     menu_id INTEGER PRIMARY KEY,
@@ -51,80 +54,86 @@ create_table_cmd_2 = <<-SQL
 SQL
 
 
-# create a kittens table (if it's not there already)
+# create tables appetizers, main_course, dessert and menu (if it's not there already)
 db.execute(create_table)
 db.execute(create_table_cmd)
 db.execute(create_table_cmd_1)
 db.execute(create_table_cmd_2)
 
-
-# add a test kitten
+# add a test appetizer
 #db.execute("INSERT INTO appetizers (appetizer_name, appetizer_description) VALUES ('hummus', 'ground garbanzo beans seasoned with garlic and roasted red chilli peppers')")
+
+# delete the test appetizer
 #db.execute("DELETE FROM appetizers WHERE appetizer_id = 1")
 
-
-# add LOOOOTS of kittens!
-# so. many. kittens.
-#KittenExplosion
-
+# Add values to the appetizer table
 def create_appetizers(db, appetizer_name, appetizer_description)
   db.execute("INSERT INTO appetizers (appetizer_name, appetizer_description) VALUES (?, ?)", [appetizer_name, appetizer_description])
 end
 
+# Add values to the main_course table
 def create_main_course(db, name, main_course_descr)
   db.execute("INSERT INTO main_course (name, main_course_descr) VALUES (?, ?)", [name, main_course_descr])
 end
 
+# Add values to the dessert table
 def create_dessert(db, dessert_name, dessert_description)
   db.execute("INSERT INTO dessert (dessert_name, dessert_description) VALUES (?, ?)", [dessert_name, dessert_description])
 end
 
+# Add values to the menu table
 def create_menu(db, app_id, main_courses_id, desserts_id)
   db.execute("INSERT INTO menu (app_id, main_courses_id, desserts_id) VALUES (?, ?, ?)", [app_id, main_courses_id, desserts_id])
 end
 
+# Select which columns to display and join the menu table with appetizers, main_course and dessert tables
 def view_menu(db)
-  db.execute("SELECT appetizers.appetizer_name, appetizers.appetizer_description, main_course.name, main_course.main_course_descr, dessert.dessert_name, dessert.dessert_description
+  db.execute("SELECT menu.menu_id, appetizers.appetizer_name, appetizers.appetizer_description, main_course.name, main_course.main_course_descr, dessert.dessert_name, dessert.dessert_description
    FROM appetizers, main_course, dessert JOIN menu ON (menu.app_id = appetizers.appetizer_id AND menu.main_courses_id = main_course.main_course_id AND menu.desserts_id = dessert.dessert_id)")
 end
 
+# Select which columns to display,join the menu table with appetizers, main_course and dessert table and provide a conditional to display specific rows
 def view_menu_1(db)
-  db.execute("SELECT appetizers.appetizer_name, appetizers.appetizer_description, main_course.name, main_course.main_course_descr, dessert.dessert_name, dessert.dessert_description
+  db.execute("SELECT menu.menu_id, appetizers.appetizer_name, appetizers.appetizer_description, main_course.name, main_course.main_course_descr, dessert.dessert_name, dessert.dessert_description
    FROM appetizers, main_course, dessert JOIN menu ON (menu.app_id = appetizers.appetizer_id AND menu.main_courses_id = main_course.main_course_id AND menu.desserts_id = dessert.dessert_id) WHERE menu.menu_id < 5 ")
 end
 
+# Select which columns to display,join the menu table with appetizers, main_course and dessert table and provide a conditional to display specific rows
 def view_menu_2(db)
-  db.execute("SELECT appetizers.appetizer_name, appetizers.appetizer_description, main_course.name, main_course.main_course_descr, dessert.dessert_name, dessert.dessert_description
+  db.execute("SELECT menu.menu_id, appetizers.appetizer_name, appetizers.appetizer_description, main_course.name, main_course.main_course_descr, dessert.dessert_name, dessert.dessert_description
    FROM appetizers, main_course, dessert JOIN menu ON (menu.app_id = appetizers.appetizer_id AND menu.main_courses_id = main_course.main_course_id AND menu.desserts_id = dessert.dessert_id) WHERE menu.menu_id > 10 AND menu.menu_id < 14")
 end
 
 
 
-
-
+# Driver Code
 puts "Welcome to Menu Management System!"
 puts "This system will help you maintain and manage your menu options!'\n'"
-puts "Enter the table name that you would like to update (appetizers/ main course/ dessert/ no update)"
+
+# Based on user input, a specific table will be selected for addition of items
+puts "Enter the table name that you would like to update (appetizers/ main course/ dessert/ no additions)"
 table_name = gets.chomp
 
 if table_name == "appetizers"
   while true
-    puts "Please enter a appetizer name!(Enter DONE to exit)"
+    puts "Please enter a Appetizer name!(Enter DONE to exit)"
     app_name = gets.chomp
     break if app_name == "DONE"
-    puts "Enter appetizer description:"
+    puts "Enter Appetizer description:"
     app_description = gets.chomp
+    # Calling method to insert values into appetizers table
     create_appetizers(db, app_name, app_description)
   end
 end
 
 if table_name == "main course"
   while true
-    puts "Please enter a Main_course name! (Enter DONE to exit)"
+    puts "Please enter a Main Course name! (Enter DONE to exit)"
     main_course_name = gets.chomp
     break if main_course_name == "DONE"
-    puts "Enter Main course description:"
+    puts "Enter Main Course description:"
     main_course_description = gets.chomp
+    # Calling method to insert values into main_course table
     create_main_course(db, main_course_name, main_course_description)
   end
 end
@@ -136,44 +145,46 @@ if table_name == "dessert"
   break if dessert_name == "DONE"
   puts "Enter Dessert description:"
   dessert_description = gets.chomp
+  # Calling method to insert values into dessert table
   create_dessert(db, dessert_name, dessert_description)
   end
 end
 
-if table_name == "no update"
+# If user selects no additons, no inserts are made to the table
+if table_name == "no additions"
   puts "The table is up to date"
 end
 
+# To select menu combinations
 puts "Please enter if you would like to set Menu options (YES / NO)"
 set_menu = gets.chomp
 if set_menu == "YES"
-  #count = 0
+  puts "To exit anytime in the program, type 0"
   while true
-    puts "To exit anytime in the program, type 0"
     puts "Enter appetizer id (1 - 10)"
     appetizer_idd = gets.to_i
+    break if appetizer_idd == 0
     puts "Enter main_course id (1 - 10)"
     main_course_idd = gets.to_i
+    break if main_course_idd == 0
     puts "Enter dessert id (1 - 10)"
     dessert_idd = gets.to_i
-    break if appetizer_idd == 0
-    break if main_course_idd == 0
     break if dessert_idd == 0
     create_menu(db, appetizer_idd, main_course_idd, dessert_idd)
-    #count += 1
-    #puts "This is menu #{count}"
   end
 
 elsif set_menu == "NO"
   puts "You chose not to set any menu options!"
 end
 
-puts "The menu options are:'\n'"
+
+
+puts "The MENU OPTIONS ARE:'\n'"
 menu_1 = view_menu(db)
 menu_2 = view_menu_1(db)
 menu_3 = view_menu_2(db)
 
-puts "The FIRST MENU OPTION IS COMPREHENSIVE."
+puts "The FIRST MENU OPTION IS A COMPREHENSIVE LIST."
 p menu_1
 puts "'\n'"
 puts "HERE IS THE SECOND MENU OPTION"
