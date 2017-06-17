d0224134e349b6cfeb746f9f7515a9ece00e8c7c
Garima Jain <garimaj2108@gmail.com>
2017-06-17 15:38:18 -0700

    Release 0

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,47 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+
+# A /contact route that displays an address
+get '/contact' do
+  "Jane works at 333 Market Street, San Fancisco!"
+end
+
+
+# A /contact route that displays an address using route parameters (extra, just to practice)
+get '/contacts/:person/:address_line_1/:city/:state/:pin' do
+  person = params[:person]
+  address_line_1 = params[:address_line_1]
+  city = params[:city]
+  state = params[:state]
+  pin = params[:pin]
+  "#{person}'s Address : #{address_line_1}, #{city}, #{state}, #{pin}."
+end
+
+# A /great_job route that says "Good job, [person's name]!", if query parameter given, else the route simply says "Good job!"
+
+get '/great_job/' do
+  person_name = params[:person_name]
+  if person_name
+    "Good job, #{person_name}"
+  else
+    "Good job!"
+  end
+end
+
+# A route that uses route parameters to add two numbers and respond with the result.
+
+get '/calculation/:first_num/:second_num' do
+  first_num = params[:first_num].to_i
+  second_num = params[:second_num].to_i
+  third_num = first_num + second_num
+  "The total for entered numbers is #{first_num} + #{second_num} =  #{third_num}"
+end
+
+# route allows user to search the database by campus
+get '/students/campus/:campus' do
+  campus = db.execute("SELECT * FROM students WHERE campus=?", [params[:campus]])
+  campus.to_s
+end
+
